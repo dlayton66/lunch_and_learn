1e61a2e8b533db1db7ecf77ca735676c0bfeeedb
Drew Layton <dlayton66@gmail.com>
2023-03-03 16:44:58 -0700

    feat/test: Get recipes by country

---
+++ spec/services/recipe_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe RecipeService, :vcr do
+  it 'gets recipes by country' do
+    response = RecipeService.get_recipes('Norway')
+
+    expect(response.status).to eq(200)
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed_response[:hits]).to be_an(Array)
+    expect(parsed_response[:hits][0][:recipe][:label]).to be_a(String)
+    expect(parsed_response[:hits][0][:recipe][:url]).to be_a(String)
+    expect(parsed_response[:hits][0][:recipe][:image]).to be_a(String)
+  end
+end
