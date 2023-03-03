89706cee994f7ff21c36a3a41be11a40ebb97204
Drew Layton <dlayton66@gmail.com>
2023-03-03 16:44:58 -0700

    feat/test: Get recipes by country

---
+++ app/services/recipe_service.rb
@@ -0,0 +1,16 @@
+class RecipeService < BaseService
+  def self.conn
+    Faraday.new(
+      url: 'https://api.edamam.com/api/recipes/v2/',
+      params: {
+        app_id: ENV['APP_ID'],
+        app_key: ENV['APP_KEY'],
+        type: 'public'
+      }
+    )
+  end
+
+  def self.get_recipes(country)
+    conn.get('', q: country)
+  end
+end
