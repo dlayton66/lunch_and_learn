d12885fb1f5249ba9e37750e1c78d1100b812a60
Drew Layton <dlayton66@gmail.com>
2023-03-03 22:00:22 -0700

    feat/test: Get recipes edge cases

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    recipes = RecipeFacade.load_recipes(params[:country])
+    recipes = RecipeFacade.load_recipes(recipe_params)
     render json: RecipeSerializer.new(recipes)
   end
+
+  private
+
+    def recipe_params
+      params.fetch(:country, [])
+    end
 end
