93fd74961eb19c9aac142115cf1a6175c8363fcd
Drew Layton <dlayton66@gmail.com>
2023-03-03 17:47:02 -0700

    fix: Add id to Recipe poro

--- app/poros/recipe.rb
+++ app/poros/recipe.rb
@@ -1,6 +1,8 @@
 class Recipe
-  attr_reader :title, :url, :country, :image
+  attr_reader :id, :title, :url, :country, :image
+
   def initialize(recipe_info, country)
+    @id = nil
     @title = recipe_info[:recipe][:label]
     @url = recipe_info[:recipe][:url]
     @country = country
