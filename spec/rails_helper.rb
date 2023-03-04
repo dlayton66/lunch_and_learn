366f64fbc019da25e2cb15a51f876b5905075f94
Drew Layton <dlayton66@gmail.com>
2023-03-03 22:10:11 -0700

    test: Filter sensitive data from VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,8 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr"
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.filter_sensitive_data('<APP_ID>') { ENV['APP_ID'] }
+  config.filter_sensitive_data('<APP_KEY>') { ENV['APP_KEY'] }
 end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
