5d2458804934beda8b70e4f3bc4fc5b29a8175b1
Drew Layton <dlayton66@gmail.com>
2023-03-06 10:53:40 -0700

    test: Rename recipe spec file

--- spec/poros/recipe_spec.rb
+++ spec/poros/recipe_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Recipe, :vcr do
   it 'has attributes' do
     recipe = RecipeFacade.load_recipes('Norway')[0]
 
+    expect(recipe.id).to be_nil
     expect(recipe.title).to be_a(String)
     expect(recipe.url).to be_a(String)
     expect(recipe.country).to eq('Norway')
