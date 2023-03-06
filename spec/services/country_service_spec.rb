88d2ccaf773b69ab179852b517f756abcc0f12d9
Drew Layton <dlayton66@gmail.com>
2023-03-06 09:25:49 -0700

    feat/test: CountryService: Get info for country

--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe CountryService, :vcr do
       expect(country[:name][:common]).to be_a(String)
     end
   end
+
+  it 'gets info for a country' do
+    response = CountryService.get_country('France')
+
+    expect(response.status).to eq(200)
+
+    country = JSON.parse(response.body, symbolize_names: true)
+
+    expect(country[0][:name][:common]).to eq('France')
+    expect(country[0][:capital][0]).to eq('Paris')
+    expect(country[0][:capitalInfo][:latlng]).to eq([48.87, 2.33])
+  end
 end
