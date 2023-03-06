3815f507bdd9c877c477059ec72cc3495626a467
Drew Layton <dlayton66@gmail.com>
2023-03-06 09:33:07 -0700

    feat/test: CountryFacade: gets capital and location

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -4,4 +4,14 @@ class CountryFacade
     parsed_response = CountryService.parse_json(response)
     parsed_response.sample[:name][:common]
   end
+
+  def self.find_capital(country)
+    response = CountryService.get_country(country)
+    parsed_response = CountryService.parse_json(response)
+
+    {
+      name: parsed_response[0][:capital][0],
+      latlng: parsed_response[0][:capitalInfo][:latlng]
+    }
+  end
 end
--- spec/facades/country_facade_spec.rb
+++ spec/facades/country_facade_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe CountryFacade, :vcr do
       expect(country).to be_a(String)
     end
   end
+
+  describe '.find_capital' do
+    it 'gets capital and location' do
+      capital = CountryFacade.find_capital('France')
+
+      expect(capital[:name]).to eq('Paris')
+      expect(capital[:latlng]).to eq([48.87, 2.33])
+    end
+  end
 end
