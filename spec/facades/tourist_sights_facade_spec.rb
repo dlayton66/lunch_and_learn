be7eef78f09a709ef15e5bd7871ed65fad3d0f9c
Drew Layton <dlayton66@gmail.com>
2023-03-06 11:02:20 -0700

    feat/test: TouristSightsFacade

---
+++ spec/facades/tourist_sights_facade_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe TouristSightsFacade, :vcr do
+  it "loads tourist sights into poros for a given country's capital" do
+    tourist_sights = TouristSightsFacade.load_tourist_sights('France')
+
+    expect(tourist_sights).to be_an(Array)
+    tourist_sights.each do |tourist_sight|
+      expect(tourist_sight.class).to be(TouristSight)
+    end
+  end
+end
