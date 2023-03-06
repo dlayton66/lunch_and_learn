c229cfdc37924579ec0c9b3e9da1622b88ca9434
Drew Layton <dlayton66@gmail.com>
2023-03-06 11:42:58 -0700

    test: Test all TouristSight poros

--- spec/poros/tourist_sight_spec.rb
+++ spec/poros/tourist_sight_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe TouristSight, :vcr do
   it 'has attributes' do
-    tourist_sight = TouristSightsFacade.load_tourist_sights('France')[0]
+    tourist_sights = TouristSightsFacade.load_tourist_sights('France')
 
-    expect(tourist_sight.id).to be_nil
-    expect(tourist_sight.name).to be_a(String)
-    expect(tourist_sight.address).to be_a(String)
-    expect(tourist_sight.place_id).to be_a(String)
+    tourist_sights.each do |tourist_sight|
+      expect(tourist_sight.id).to be_nil
+      expect(tourist_sight.name).to be_a(String)
+      expect(tourist_sight.address).to be_a(String)
+      expect(tourist_sight.place_id).to be_a(String)
+    end
   end
 end
