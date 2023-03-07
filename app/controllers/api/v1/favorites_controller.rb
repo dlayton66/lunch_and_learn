c7aa09d85cedc02aa0b655fea258b11080e03d71
Drew Layton <dlayton66@gmail.com>
2023-03-07 14:38:54 -0700

    feat/test: Get a user's favorites

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,14 +1,22 @@
 class Api::V1::FavoritesController < ApplicationController
+  before_action :find_user
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_api_key
 
   def create
-    user = User.find_by!(api_key: params[:api_key])
-    if user.favorites.create!(favorite_params)
+    if @user.favorites.create!(favorite_params)
       render json: { success: "Favorite added successfully" }, status: 201
     end
   end
 
+  def index
+    render json: FavoriteSerializer.new(@user.favorites)
+  end
+
   private
+
+    def find_user
+      @user = User.find_by!(api_key: params[:api_key])
+    end
 
     def favorite_params
       params.permit(:country, :recipe_link, :recipe_title)
--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'favorites request' do
       recipe_link: "https://www.recipelink.com",
       recipe_title: "Crab Fried Rice"
     } }
-    
+
     it 'creates a favorite recipe for a user' do
 
       expect(Favorite.count).to eq(0)
@@ -41,14 +41,8 @@ RSpec.describe 'favorites request' do
 
     describe 'sad path' do
       context 'bad api key' do
-        let(:payload) { {
-          api_key: 'bad_key',
-          country: "Thailand",
-          recipe_link: "https://www.recipelink.com",
-          recipe_title: "Crab Fried Rice"
-        } }
-
         it 'returns an error' do
+          payload[:api_key] = 'bad_key'
           post api_v1_favorites_path, headers: headers, params: JSON.generate(payload)
 
           expect(response.status).to eq(404)
@@ -56,6 +50,58 @@ RSpec.describe 'favorites request' do
           parsed_response = JSON.parse(response.body, symbolize_names: true)
 
           expect(parsed_response[:errors][0][:detail]).to eq("Couldn't find User")
+        end
+      end
+    end
+  end
+
+  describe 'index' do
+    it 'returns favorites for a user' do
+      favorites = create_list(:favorite, 3, user_id: user.id)
+      get api_v1_favorites_path, params: { api_key: user.api_key }
+
+      expect(response.status).to be(200)
+
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_response[:data]).to be_an(Array)
+      parsed_response[:data].each.with_index do |favorite, i|
+        expect(favorite[:id]).to eq(favorites[i].id.to_s)
+        expect(favorite[:type]).to eq('favorite')
+
+        expect(favorite[:attributes][:country]).to eq(favorites[i].country)
+        expect(favorite[:attributes][:recipe_link]).to eq(favorites[i].recipe_link)
+        expect(favorite[:attributes][:recipe_title]).to eq(favorites[i].recipe_title)
+        expect(favorite[:attributes][:created_at]).to eq(favorites[i].created_at.as_json)
+      end
+    end
+
+    describe 'edge cases' do
+      context 'user has no favorites' do
+        it 'data object is empty array' do
+          get api_v1_favorites_path, params: { api_key: user.api_key }
+
+          expect(response.status).to be(200)
+    
+          parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+          expect(parsed_response[:data]).to be_an(Array)
+          expect(parsed_response[:data]).to be_empty
+        end
+      end
+    end
+
+    describe 'sad path' do
+      context 'bad api key' do
+        it 'returns an error' do
+          get api_v1_favorites_path, params: { api_key: 'bad_key' }
+
+          expect(response.status).to eq(404)
+          
+          parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+          expect(parsed_response[:errors][0][:detail]).to eq("Couldn't find User")
+
         end
       end
     end
