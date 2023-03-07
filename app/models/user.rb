3bc5f338a6c2acafaa56502cc3b2887d0eee7d4d
Drew Layton <dlayton66@gmail.com>
2023-03-07 11:07:18 -0700

    feat/test: User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  before_validation :generate_api_key
+
   validates :name, presence: true
   validates :email, uniqueness: true, presence: true
 
   has_secure_password
+
+  private
+
+    def generate_api_key
+      self.api_key = SecureRandom.hex
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe User do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
   end
+
+  describe 'callbacks' do
+    describe '.generate_api_key' do
+      it 'generates an api key on user creation' do
+        user = User.create!(
+          name: 'Drew', 
+          email: 'drew@drew.com',
+          password: 'password123',
+          password_confirmation: 'password123'
+        )
+
+        expect(user.api_key).to be_a(String)
+      end
+    end
+  end
 end
