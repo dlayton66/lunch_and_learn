26103a8fc5e91e9aeb5a9d00eb194aea49ac4df2
Drew Layton <dlayton66@gmail.com>
2023-03-07 06:03:45 -0700

    test: Add some describe blocks

--- spec/services/countries_service_spec.rb
+++ spec/services/countries_service_spec.rb
@@ -1,29 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe CountriesService, :vcr do
-  it 'gets a list of all countries' do
-    response = CountriesService.get_countries
-
-    expect(response.status).to eq(200)
-
-    countries = JSON.parse(response.body, symbolize_names: true)
-
-    expect(countries).to be_an(Array)
-
-    countries.each do |country|
-      expect(country[:name][:common]).to be_a(String)
+  describe '.get_countries' do
+    it 'gets a list of all countries' do
+      response = CountriesService.get_countries
+  
+      expect(response.status).to eq(200)
+  
+      countries = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(countries).to be_an(Array)
+  
+      countries.each do |country|
+        expect(country[:name][:common]).to be_a(String)
+      end
     end
   end
 
-  it 'gets info for a country' do
-    response = CountriesService.get_country('France')
-
-    expect(response.status).to eq(200)
-
-    country = JSON.parse(response.body, symbolize_names: true)
-
-    expect(country[0][:name][:common]).to eq('France')
-    expect(country[0][:capital][0]).to eq('Paris')
-    expect(country[0][:capitalInfo][:latlng]).to eq([48.87, 2.33])
+  describe '.get_country' do
+    it 'gets info for a country' do
+      response = CountriesService.get_country('France')
+  
+      expect(response.status).to eq(200)
+  
+      country = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(country[0][:name][:common]).to eq('France')
+      expect(country[0][:capital][0]).to eq('Paris')
+      expect(country[0][:capitalInfo][:latlng]).to eq([48.87, 2.33])
+    end
   end
 end
--- spec/services/tourist_sights_service_spec.rb
+++ spec/services/tourist_sights_service_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe TouristSightsService, :vcr do
-  it "gets tourist sights for given latitude and longitude" do
-    response = TouristSightsService.get_tourist_sights([48.87, 2.33])
-
-    expect(response.status).to eq(200)
-
-    parsed_response = JSON.parse(response.body, symbolize_names: true)
-
-    expect(parsed_response[:type]).to eq('FeatureCollection')
-    expect(parsed_response[:features][0][:type]).to eq('Feature')
-    expect(parsed_response[:features][0][:properties][:name]).to be_a(String)
-    expect(parsed_response[:features][0][:properties][:formatted]).to be_a(String)
-    expect(parsed_response[:features][0][:properties][:place_id]).to be_a(String)
+  describe '.get_tourist_sights' do
+    it "gets tourist sights for given latitude and longitude" do
+      response = TouristSightsService.get_tourist_sights([48.87, 2.33])
+  
+      expect(response.status).to eq(200)
+  
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(parsed_response[:type]).to eq('FeatureCollection')
+      expect(parsed_response[:features][0][:type]).to eq('Feature')
+      expect(parsed_response[:features][0][:properties][:name]).to be_a(String)
+      expect(parsed_response[:features][0][:properties][:formatted]).to be_a(String)
+      expect(parsed_response[:features][0][:properties][:place_id]).to be_a(String)
+    end
   end
 end
