b3dcb55bc09d0d7b0b0853c1a3a6ed36bceb68c4
Drew Layton <dlayton66@gmail.com>
2023-03-07 12:30:12 -0700

    feat: Add favorites table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_07_162924) do
+ActiveRecord::Schema.define(version: 2023_03_07_192816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "country"
+    t.string "recipe_link"
+    t.string "recipe_title"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +32,5 @@ ActiveRecord::Schema.define(version: 2023_03_07_162924) do
     t.string "api_key"
   end
 
+  add_foreign_key "favorites", "users"
 end
